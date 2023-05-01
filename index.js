75eb73cd357f6186de869627544fad38eac2293d
Delowar Hossain <bscdelowar@yahoo.com>
2023-05-02 01:31:46 +0600

    load json data from index

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express=require("express")
 const app=express()
 const port=5000
 const cors=require('cors')
-
+const chefsData=require('./data/chefsData.json')
 app.use(cors())
 app.listen(port, ()=>{
     console.log(`Example app listening on port ${port}`)
 })
 app.get('/', (req,res)=>{
-    res.send('Server cunnection ok')
+    res.send(chefsData)
 })
